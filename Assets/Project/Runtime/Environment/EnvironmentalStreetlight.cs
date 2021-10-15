11cf0e61ba49314d915aabadced7e7d13f6d5430
WoolyDrool <nikolai.mcneely15@gmail.com>
2021-10-15 10:53:18 -0700

    Added functionality for streetlights turning on/off depending on TOD

---
+++ Assets/Project/Runtime/Environment/EnvironmentalStreetlight.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using Project.Runtime.Global;
+
+namespace Project.Runtime.Environment
+{
+    public class EnvironmentalStreetlight : MonoBehaviour
+    {
+        public GameObject lightObj;
+        public TimeManager.TimeOfDay enableTime;
+        public TimeManager.TimeOfDay disableTime;
+
+        private TimeManager _time;
+
+        private void Start()
+        {
+            _time = GameManager.instance.timeManager;
+        }
+
+        private void Update()
+        {
+            if (_time)
+            {
+                if (_time.time == enableTime)
+                {
+                    lightObj.SetActive(true);
+                }
+
+                if (_time.time == disableTime || _time.time != enableTime)
+                {
+                    lightObj.SetActive(false);
+                }
+            }
+        }
+    }
+}
