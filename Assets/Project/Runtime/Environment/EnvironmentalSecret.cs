caea3817c3870def2071469e3f3c1fb1c284d409
WoolyDrool <nikolai.mcneely15@gmail.com>
2021-10-15 21:33:49 -0700

    Added sound functionality to EnvironmentalSecret

--- Assets/Project/Runtime/Environment/EnvironmentalSecret.cs
+++ Assets/Project/Runtime/Environment/EnvironmentalSecret.cs
@@ -1,16 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Project.Runtime.Gameplay.Inventory;
+using Project.Runtime.UI.Elements;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class EnvironmentalSecret : MonoBehaviour
 {
+    public AudioClip discoverSound;
+    public AudioMixerGroup MixerGroup;
+    private PlayerInventory _inventory;
     void Start()
     {
-        
+        _inventory = GameManager.instance.playerInventory;
     }
 
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameManager.instance.audioManager.PlaySoundOnce(discoverSound, MixerGroup);
+            UIStatusUpdate.update.AddStatusMessage(UpdateType.SECRET, "You found a secret!");
+            _inventory.currentSecrets++;
+        }
     }
 }
