dba40d1c3f91bd2949face5a9ac69301380ad6b8
WoolyDrool <nikolai.mcneely15@gmail.com>
2021-10-18 09:19:01 -0700

    Made TweenMoveToPosition slightly more flexible

--- Assets/Project/Runtime/Utility/LeanTween Tools/TweenMoveToPosition.cs
+++ Assets/Project/Runtime/Utility/LeanTween Tools/TweenMoveToPosition.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Vector3 = System.Numerics.Vector3;
-
 public class TweenMoveToPosition : MonoBehaviour
 {
     public Transform positionToMoveTo;
     public LTBezierPath path;
     public float speed;
     private LTDescr _tweenObject;
+    public Vector3 rotation;
+    public bool startImmediately;
+    public bool destroyOnComplete = true;
     void Start()
     {
-        _tweenObject = LeanTween.move(gameObject, positionToMoveTo.position, speed);
-        _tweenObject.destroyOnComplete = true;
+        if(startImmediately)
+            DoTween();
     }
 
     void Update()
     {
         
+    }
+
+    public void DoTween()
+    {
+        _tweenObject = LeanTween.move(gameObject, positionToMoveTo.position, speed);
+        _tweenObject.destroyOnComplete = destroyOnComplete;
     }
 }
