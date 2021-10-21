e42eb03de071d15915d10c6ad13f757d1acbad02
WoolyDrool <nikolai.mcneely15@gmail.com>
2021-10-21 12:47:46 -0700

    Switched project from URP to SRP

--- Assets/Project/Runtime/UI/Buttons/GraphicsSettings/UIButtonGSPPDL.cs
+++ Assets/Project/Runtime/UI/Buttons/GraphicsSettings/UIButtonGSPPDL.cs
@@ -2,21 +2,17 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
 
 public class UIButtonGSPPDL : MonoBehaviour
 {
-    public UniversalRenderPipelineAsset urpAsset;
     public TextMeshProUGUI text;
     public void UpValue()
     {
-        urpAsset.maxAdditionalLightsCount++;
-        text.text = urpAsset.maxAdditionalLightsCount.ToString();
+        
     }
 
     public void DownValue()
     {
-        urpAsset.maxAdditionalLightsCount--;
-        text.text = urpAsset.maxAdditionalLightsCount.ToString();
+       
     }
 }
