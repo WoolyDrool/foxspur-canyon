517d29e4966af164a7fe9827babe99f15d008d5a
WoolyDrool <nikolai.mcneely15@gmail.com>
2021-10-25 11:42:44 -0700

    Added temporary exit trail code

--- Assets/Project/Runtime/Gameplay/Interactables/Trails/InteractableTrailExit.cs
+++ Assets/Project/Runtime/Gameplay/Interactables/Trails/InteractableTrailExit.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Project.Runtime.Gameplay.Interactables;
+using Project.Runtime.UI.Elements;
 using UnityEngine;
 
 public class InteractableTrailExit : MonoBehaviour
 {
+    public InteractableTrailScoreManager trailManager;
     void Start()
     {
         
@@ -12,5 +15,17 @@ public class InteractableTrailExit : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnInteract()
+    {
+        if (trailManager.status >= TrailStatus.MINIMUM)
+        {
+            //Do exfill code
+        }
+        else
+        {
+            UIAlertUpdate.alert.AddAlertMessage(AlertType.GENERAL, "Trail not finished!");
+        }
     }
 }
