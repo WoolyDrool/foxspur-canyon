46312ea903f8aa9a6a5744490211ba168c2a6e69
WoolyDrool <nikolai.mcneely15@gmail.com>
2021-11-22 11:08:13 -0800

    Added TrailData SO type

---
+++ Assets/Project/Runtime/Gameplay/Interactables/Trails/TrailData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Project.Runtime.Gameplay.Interactables
+{
+    public class TrailData : ScriptableObject
+    {
+        public string TrailName;
+        public int Items;
+        public int Secrets;
+        public bool Completed;
+    }
+}
