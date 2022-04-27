7ecf7a9bc1bee0720ff126c216e4bc1165551622
WoolyDrool <nikolai.mcneely15@gmail.com>
2022-04-27 11:15:51 -0700

    Added "MetaState" enum to help track when to enable the main PlayerController component

--- Assets/Project/Runtime/Control/Serialization/PersistentPlayerCharacter.cs
+++ Assets/Project/Runtime/Control/Serialization/PersistentPlayerCharacter.cs
@@ -1,9 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Project.Runtime.Global;
+using Project.Runtime.Serialization;
+using UnityEditor;
 using UnityEngine;
 
 public class PersistentPlayerCharacter : MonoBehaviour
 {
+    public PlayerProfile Profile;
+    public static PersistentPlayerCharacter ppc;
+    public GameObject playerController;
+    public MetaState metaState = MetaState.AWAKE;
+    public enum MetaState
+    {
+        AWAKE,
+        LOAD,
+        VOID
+    };
+    private void Awake()
+    {
+        ppc = this;
+        DontDestroyOnLoad(this);
+        //SceneLoadingManager.loader.LoadSceneAdditive(Profile.lastVisitedScene);
+    }
+
     void Start()
     {
         
@@ -11,6 +32,6 @@ public class PersistentPlayerCharacter : MonoBehaviour
 
     void Update()
     {
-        
+        playerController.SetActive(metaState == MetaState.AWAKE);
     }
 }
