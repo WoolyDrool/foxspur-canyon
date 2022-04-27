3dc0cb2c41b0c71bdc3950a6ef67895403715d6c
WoolyDrool <nikolai.mcneely15@gmail.com>
2022-04-27 11:01:11 -0700

    Added some framework functions for eventual quest objects

--- Assets/Project/Runtime/Gameplay/Systems/GenericQuestObject.cs
+++ Assets/Project/Runtime/Gameplay/Systems/GenericQuestObject.cs
@@ -1,14 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Project.Runtime.Gameplay.Interactables;
 
 namespace Project.Runtime.Gameplay.Systems
 {
     [CreateAssetMenu(menuName = "Game/Quests/New Quest")]
     public class GenericQuestObject : ScriptableObject
     {
-        public string QuestName;
-        public string QuestDescription;
-    }
+        public string questName;
+        public string questDescription;
+        public QuestType questType;
+        public enum QuestType
+        {
+            TRAIL,
+            SIDE,
+            MISC
+        };
 
+        public QuestStatus questStatus;
+        public enum QuestStatus
+        {
+            NOT_STARTED,
+            IN_PROGRESS,
+            COMPLETE
+        };
+
+        public int currentStage;
+        public int numberOfStages;
+
+        public TrailData trailData;
+
+        public void DetermineCurrentStage()
+        {
+            if (currentStage >= numberOfStages)
+            {
+                questStatus = QuestStatus.COMPLETE;
+            }
+        }
+
+        public void StartQuest()
+        {
+           
+        }
+        
+        public void PauseQuest()
+        {}
+        
+        public void ResumeQuest()
+        {}
+
+        public void FinishQuest()
+        {}
+        
+    }
 }
